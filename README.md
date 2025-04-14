6c91f5daf8fc150dc6208d3d7952f7d72ebd3a0b
RDaveML <d.m.leitritz@vu.nl>
2025-04-14 15:48:46 +0200

    readme updated (2025-04-13), PCA script for PGS finished

--- README.md
+++ README.md
@@ -9,20 +9,104 @@ Authors: Leitritz, D; Pool, R.; Ligthart, L.; Bartels, M.; Pelt, D.
 Department of Biological Psychology; Vrije Universiteit Amsterdam; Amsterdam,. Noord-Holland; The Netherlands
 July 2024
 
+
+### Content repository
 This repository contains all documents relevant to the analysis and is organized as follows:
 
 - data contains only proof-of-concept calculations and otherwise falls under the gitignore
 - doc contains all documentation of the study including legal documents and the pre-registration, furthermore visualizations based on the results of the analyses
 - scripts contains all R, Python, Mplus and bash scripts that were used for the analysis
 
+#### Important coding and variable information: 
+- CBCL questions: 
+    - -1 == missing
+    - 0 = not true;
+    - 1 = somewhat true;
+    - 2 = very true
+     (items *q13m5*; *q11m5* and *q4m5* were recoded to align with the coding)
+
+- all questions have specific ending per age:
+    - age 3 = **m3**
+    - age 5 = **m5**
+    - age 7 = **m7**
+    - age 10 = **m10**
+    - age 12 = **m12**
+    - age 14 = **ysr14**
+    - age 16 = **ysr16**
+
+- sex: 3 unique codes
+    -  -9 == known or suspected transgender;
+    -  -2 == missing inconsistent data;
+    -  -1 == missing no data;
+    -  1 == male;
+    -  2 == female*
+
+- twzyg: zygosity twin 1 vs twin 2; 6 unique codes
+    -  1 == MZM;
+    -  2 == DZM;
+    -  3 == MZF;
+    -  4 == DZF;
+    -  5 == DOSmf (dizygotic and different sex, participant is male);
+    -  6 == DOSfm (dizygotic and different sex, participant is female)
+
+- ea4fa_agg: father's educational attainment - 4 levels:
+    -  -1 == missing;
+    -  1 == lager onderwijs;
+    -  2 == lager beroepsonderwijs (lbo) / middelbaar algemeen onderwijs (lavo, mavo);
+    -  3 == middelbaar beroepsonderwijs (mbo) / voorgezet algemeen onderwijs (havo, vwo);
+    -  4 == hoger beroepsonderwijs (hbo), wetenschappelijk onderwijs (wo)
+
+- ea4mo_agg: mother's educational attainment - 4 levels:
+    -  -1 == missing;
+    -  1 == lager onderwijs;
+    -  2 == lager beroepsonderwijs (lbo) / middelbaar algemeen onderwijs (lavo, mavo);
+    -  3 == middelbaar beroepsonderwijs (mbo) / voorgezet algemeen onderwijs (havo, vwo);
+    -  4 == hoger beroepsonderwijs (hbo), wetenschappelijk onderwijs (wo)
+
+- invd8: date of completion wave 8 (**invd = invuldate**), can be used for age calculation and time lag
+
+- invjrm3: Year of filling in survey wave 3 (by mother) (**invjrm = invuljaar moeder**)
+
+
+#### Content scripts
 The project contains the following areas of coding and analysis:
-- Preprocessing
-- Latent growth / longidtudinal modeling
-- Machine learning preprocessing
-- Running machine learning models
+- Preprocessing / filtering / train-test-splitting
+- Latent growth / longitudinal modeling
+- Preparation and running machine learning models
 - assessment of model stability
 - model evaluation
 
+(The following section is constantly being updated!)
+Scripts contain the following operations (in the following order):
+01 - 01_participantIDs.R
+02 - 02_data_exploration.R
+03 - 03_covariates.R
+04 - 04_data_cleaning_filtering1.R
+05 - 05_initial_split_training_test_data.R
+06 - 06_longitudinal_features_no_LGM.R
+06a - 06_a_autocorrelation.R
+06b - 06_b_merge_nonLGM.R
+07 - 07_LGM.R
+08 - *old: 08a_ML_model_A.R (not needed anymore)* will be changed to **08_PCA_PRS_NTR.R**
+09 - 09_0_run_bootstrap_stability.R
+(09a - Revision run bootstrap with further feature space shrinkage!)
+**Also add this at all other bootstrapping model steps**
+10 - 10_inspection_bootstrap_stability_model_0.R
+11 - *no filename yet* (run bootstrap stability model A)
+12 - *no filename yet* (inspection bootstrap stability model A)
+13 - *no filename yet, tentative depending on discussion if new training test split* (new training-test split, also bootstrap for PGS data, includes removing the genetic outliers!)
+14 - *no filename yet* (run bootstrap stability model B: CBS only)
+15 - *no filename yet* (inspect bootstrap stability model B)
+16 - *no filename yet* (run bootstrap stability mode C: CBCL features + non-LGM CBCL features + PGS)
+***16 - no filename yet* (this is still somewhat tricky, run bootstrap with LGM features, but when calculating these, only do it once with initial test set? Still to be discussed once Mplus code is completely ready, add to documentation!)***
+17 - *no filename yet* (run bootstrap stability model D: raw CBCL scores + non-LGM features + LGM features, use same split as in 05_initial_split_training_test_data.R)
+18 - *no filename yet* (inspect bootstrap stability model D)
+19 - *no filename yet* (run bootstrap stability model E: all features)
+20 - *no filename yet* (inspect bootstrap stability model E)
+21 onwards: ML analyses, feature importances; alternative, after all stability checks are done!
+
+
+#### Data availability
 Requests for obtaining the raw data need to be sent directly to the NTR (https://ntr-data-request.psy.vu.nl/). A data request needs to
 state for which purposes the data are requested and which safety measures will be taken. Additionally, an analysis or replication plan
 needs to be appended to the data request.
---
+++ scripts/08_PCA_PRS_NTR.R
@@ -0,0 +1,177 @@
+## This script is supposed to execute the following steps (in the following order):
+## Unzip .zip file in the directory /data/dleitritz/p01_CBCL_PGS_LGM/data/intermediate/PGS
+## for every .sav file in the unzipped directory:
+## 1. Read in the .sav file
+## 2. Perform specific calculations: Calculate a principle component analysis (PCA) on all columns that contain "PGS" in their name
+## 3. Save the values of the 1st Principle component together with the ID and all specified covariates,
+## the column name of the first principal_component should consist out of the 
+## character string "PC1_" and the phenotype which is contained in the filename of the .sav file
+## after all calculations are done, merge all dataframes on the ID columns, named "FISNumber"
+## finally, save the dataset as a .csv file in the directory /data/dleitritz/p01_CBCL_PGS_LGM/data/intermediate/PGS
+
+# Load necessary libraries
+pacman::p_load("dplyr", "haven", "foreign", "here", "readr",
+               "stringr", "readxl", "data.table", "caret", "car", "glmnet",
+               "ParBayesianOptimization", "ranger", "e1071", "randomForestSRC",
+               "xgboost", "parallel", "doParallel", "fastDummies", "RANN",
+               "kernlab", "ggplot2", "purrr", "tidyr", "rvest", "psych")
+
+# Unzip the .zip file in the current directory
+## The zip files are in the directory /data/dleitritz/p01_CBCL_PGS_LGM/data/intermediate/PGS
+unzip_dir <- "data/intermediate/PGS/unzipped_files"
+zip_dir <- "/data/dleitritz/p01_CBCL_PGS_LGM/data/intermediate/PGS"  # Directory containing zip files
+zip_file <- list.files(path = zip_dir, pattern = "\\.zip$", full.names = TRUE)
+if (length(zip_file) == 1) {
+    unzip(zip_file, exdir = unzip_dir)
+} else {
+    stop("No .zip file found or multiple .zip files detected.")
+}
+
+# List all .sav files in the unzipped directory
+sav_files <- list.files(unzip_dir, pattern = "\\.sav$", full.names = TRUE)
+
+## sandboxing here with only the first element from sav_files
+test_file <- sav_files[1]
+## loading in data
+data <- read.spss(test_file, to.data.frame = TRUE, use.value.labels = FALSE)
+print(colnames(data))
+str(data)
+
+## check genetic outliers (variable: EUR_1KG)
+table(data$EUR_1KG_Outlier)
+## make table of proportions of genetic outliers
+table(data$EUR_1KG_Outlier) / nrow(data)
+
+## check for missing values
+sum(colMeans(is.na(data)))
+## only column SEX_add1 has missings (all na!)
+
+## saving genetic covariate names in vector
+gen_covariates <- c("PLD_AXIOM", "PLD_GSA", 
+                    grep("PC",
+                         grep("1KG", colnames(data), value = TRUE),
+                         value = TRUE))
+
+## saving names of outlier columns in vector
+outlier_cols <- c("EUR_1KG_Outlier", "NL_Strict_Outlier")
+
+## Check: Are genetic covariates equal across files?
+data1 <- read.spss(sav_files[2], to.data.frame = TRUE, use.value.labels = FALSE)
+
+## checking if column PC1_1KG has exactly the same values in both datasets
+all(data$PC1_1KG == data1$PC1_1KG)
+## checking if column FISNumber has exactly the same values in both datasets
+all(data$FISNumber == data1$FISNumber)
+
+
+## Selecting the score columns for PCA
+score_cols <- grep("SCORE", colnames(data), value = TRUE)
+
+## carry out PCA (pca function from psych package)
+pca_PGS <- pca(data[, score_cols])
+pca_PGS <- pca_PGS$scores[, 1]
+
+data.frame(scores = pca_PGS) %>%
+    ggplot(aes(x = scores)) + 
+    geom_histogram()
+
+
+# Function to perform specific calculations on a .sav file
+process_sav_pca <- function(file_path) {
+    data <- read.spss(file_path, to.data.frame = TRUE)
+    
+    ## needed: rename column 'FISnumber' to 'FISNumber'
+    colnames(data)[colnames(data) == "FISnumber"] <- "FISNumber"
+
+    ## saving genetic covariate names in vector
+    gen_covariates <- c("PLD_AXIOM", "PLD_GSA", 
+                        grep("PC",
+                            grep("1KG", colnames(data), value = TRUE),
+                                 value = TRUE))
+    
+    ## saving names of outlier columns in vector
+    outlier_cols <- c("EUR_1KG_Outlier", "NL_Strict_Outlier")
+
+                                 
+
+    ## also taking covariates and outlier columns in first iteration
+    if(file_path == sav_files[1]){
+       PCA_PGS_data <- data %>%
+        select(FISNumber, all_of(c(gen_covariates, outlier_cols)),
+               contains("SCORE"))
+        ## otherwise only FISNumber and SCORE columns
+    } else {
+       PCA_PGS_data <- data %>%
+        select(FISNumber, contains("SCORE"))
+    }
+
+    ## carrying out PCA (without intermediate objects, only scores of interest)
+    pca_PGS <- pca(PCA_PGS_data %>%
+       select(contains("SCORE")))$scores[, 1]
+    
+    ## obtaining the phenotype from the filename:
+    ## This should be always the part of the filename that comes between 
+    ## the string 'NTR-DSR-4552_' and the next underscore in the filename
+    phenotype <- str_extract(file_path, "(?<=NTR-DSR-4552_)[^_]+")
+    
+    ## Still address this issue: ADHD, AlzheimersDisease and Smoking 
+    ## exist several times! Find out which phenotypes those exactly are!
+    if(file_path == sav_files[1]){
+        data_out <- PCA_PGS_data %>%
+            select(FISNumber, all_of(c(gen_covariates, outlier_cols)))
+        data_out$PC1_PGS <- pca_PGS
+        colnames(data_out)[colnames(data_out) == "PC1_PGS"] <- 
+            paste0("PC1_PGS_", phenotype)
+    } else {
+        data_out <- PCA_PGS_data %>%
+            select(FISNumber)
+        data_out$PC1_PGS <- pca_PGS
+        ## renaming the column with the PGS score to include the phenotype,
+        ## since ADHD, AlzheimersDisease and Smoking occur multiple times in
+        ## file names, they are handled separately
+        colnames(data_out)[colnames(data_out) == "PC1_PGS"] <- 
+            case_when(
+    file_path == "data/intermediate/PGS/unzipped_files/NTR-DSR-4552_ADHD_PMID32026073_MRG18_PedMergedWithScores.sav"
+        ~ "PC1_PGS_ADHD_childhood",
+    file_path == "data/intermediate/PGS/unzipped_files/NTR-DSR-4552_ADHD_PMID36702997_MRG18_PedMergedWithScores.sav"
+        ~ "PC1_PGS_ADHD",
+    file_path == "data/intermediate/PGS/unzipped_files/NTR-DSR-4552_AlzheimersDisease_PMID24162737_MRG18_PedMergedWithScores.sav"
+        ~ "PC1_PGS_ADHD_AlzheimersDisease_Lambert_2013",
+    file_path == "data/intermediate/PGS/unzipped_files/NTR-DSR-4552_AlzheimersDisease_PMID35379992_MRG18_PedMergedWithScores.sav"
+        ~ "PC1_PGS_ADHD_AlzheimersDisease_Bellenguez_2022",
+    file_path == "data/intermediate/PGS/unzipped_files/NTR-DSR-4552_Smoking_CigarettesPerDay_PMID30643251_MRG18_PedMergedWithScores.sav" 
+        ~ "PC1_PGS_SmokingCigarettesPerDay",
+    ## in the GWAS reference PMID31427789, smoking was coded categorically
+    ## (0 = never, 1 = previous, 2 = current)
+    file_path == "data/intermediate/PGS/unzipped_files/NTR-DSR-4552_Smoking_PMID31427789_MRG18_PedMergedWithScores.sav"
+        ~ "PC1_PGS_SmokingCategorical",
+    file_path == "data/intermediate/PGS/unzipped_files/NTR-DSR-4552_Smoking_SmokingCessation_PMID30643251_MRG18_PedMergedWithScores.sav"
+        ~ "PC1_PGS_SmokingCessation",
+    file_path == "data/intermediate/PGS/unzipped_files/NTR-DSR-4552_Smoking_SmokingInitiation_PMID30643251_MRG18_PedMergedWithScores.sav"
+        ~ "PC1_PGS_SmokingInitiation", 
+    .default = paste0("PC1_PGS_", phenotype)  
+            )
+    }
+
+    return(data_out)
+}
+
+# Process each .sav file and store results
+full_data_PCA_PGS <- lapply(sav_files, process_sav_pca)
+
+# Merge all dataframes on the ID column "FISNumber"
+merged_data <- Reduce(function(x, y) merge(x, y, by = "FISNumber", all = TRUE),
+                      full_data_PCA_PGS)
+
+# head(merged_data)
+# colnames(merged_data)
+# table(merged_data$EUR_1KG_Outlier) / nrow(merged_data)
+# ~3.7% EUR outliers
+# table(merged_data$NL_Strict_Outlier) / nrow(merged_data)
+# ~5% NL outliers
+# sum(colMeans(is.na(merged_data)))
+# No missing values
+
+## saving data
+saveRDS(merged_data,
+        file = here::here("data", "intermediate", "PGS", "data_PCA_PGS.rds"))
